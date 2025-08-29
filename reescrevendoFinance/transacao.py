ad0589756268f71cddb025614b997a80dd979052
Daniel Augusto <danielamsouza00@gmail.com>
2025-08-28 23:32:03 -0300

    Update transacao.py

--- reescrevendoFinance/transacao.py
+++ reescrevendoFinance/transacao.py
@@ -7,13 +7,5 @@ class Transacao:
     valor: float
     categoria: Categoria
 
-    def editar_transacao(self, dado, valor):
-        if dado == 'tipo':
-            self.tipo = valor
-        elif dado == 'valor':
-            self.valor = valor
-        else:
-            self.categoria = Categoria(valor)
-
     def exibir_transacao(self):
         print(f'Tipo de transação: {self.tipo} || Valor: {self.valor} || Categotia: {self.categoria}')
